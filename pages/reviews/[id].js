e412b20b839da658e9e96833c7d8b6bd04753a67
STopalovv <stopalovv@gmail.com>
2022-09-03 18:27:48 +0300

    Adding dynamic routing page: getStaticPaths

--- pages/reviews/[id].js
+++ pages/reviews/[id].js
@@ -3,3 +3,18 @@ export default function Review() {
         <div>...</div>
     )
 }
+
+export async function getStaticPaths() {
+    const reviews = await fetch('/.netlify/functions/reviews')
+    const paths = reviews.map(review => {
+        return {
+            params: {
+                id: review.title
+            }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
